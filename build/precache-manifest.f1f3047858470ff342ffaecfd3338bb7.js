3276637edf4e942ebaa35462ed53cc6599a6bbfe
RV <rgvegajr@gmail.com>
2020-03-13 23:14:06 -0400

    created build folder

---
+++ build/precache-manifest.f1f3047858470ff342ffaecfd3338bb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc7fe229da62941e900581e1b60a1cff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e00965b278a614595e81",
+    "url": "/static/css/2.6010d4dc.chunk.css"
+  },
+  {
+    "revision": "e00965b278a614595e81",
+    "url": "/static/js/2.c11e17e9.chunk.js"
+  },
+  {
+    "revision": "d8c58b6348a8bcdb5581690a4740e904",
+    "url": "/static/js/2.c11e17e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e144ef1b4cc0547348e",
+    "url": "/static/js/main.b01d8f96.chunk.js"
+  },
+  {
+    "revision": "565c961092d1710fd7e8",
+    "url": "/static/js/runtime-main.f2903230.js"
+  }
+]);
